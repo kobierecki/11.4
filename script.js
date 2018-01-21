69adb6a67f1266c08abe33b53dc3a280a4033179
Oskar Kobierecki <oskar.kobierecki@o2.pl>
2018-01-22 00:38:14 +0100

    prototyp

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ function Phone(brand, price, color) {
     this.brand= brand;
     this.price= price;
     this.color = color;
-    this.getWarantyCost = function(){
-        return 0.1 * price;
-    }
 }
 
-Phone.prototype.printinfo = function() {
-    console.log("The phone brand is " + this.brand + ", color is " + this.color + ", the price is " + this.price + "extended waranty cost: " + this.getWarantyCost());
+Phone.prototype.GetWaranty = function(){
+        return 0.1 * this.price;
+}
+
+Phone.prototype.printinfo = function() {  
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + ", the price is " + this.price + " extended waranty cost: " + this.GetWaranty());
 }
 
 var iPhone6s = new Phone("Apple", 2250, "silver");
